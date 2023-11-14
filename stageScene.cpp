a44be4b1c63025bdff8cd0d5f69298b608870eea
k022g0009@g.neec.ac.jp <k022g0009@g.neec.ac.jp>
2023-11-14 14:25:00 +0900

    ちょっとしたシューティング完成

--- stageScene.cpp
+++ stageScene.cpp
@@ -1,19 +1,53 @@
 #include "StageScene.h"
 
 void StageScene::Initialize() {
-	// キー入力を受け取る
-	memcpy(preKeys, keys, 256);
-	Novice::GetHitKeyStateAll(keys);
+	playerPos = { 640,360 };
+	enemyPos = { 640,0 };
+	bulletPos = { playerPos.x,playerPos.y };
+
+	bulletSize = 20;
+	enemySize = 50;
+
+	speed = 5.0f;
+
+	isAlive = true;
+	isBulletShot = false;
+
 }
 
 void StageScene::Update() {
-	Initialize();
+	// キー入力を受け取る
+	memcpy(preKeys, keys, 256);
+	Novice::GetHitKeyStateAll(keys);
+
 	if (preKeys[DIK_SPACE] == 0 && keys[DIK_SPACE]) {
+		isBulletShot = true;
+	}
+
+	if (isBulletShot == true) {
+		bulletPos.y -= speed;
+	}
+
+	//色の変換
+	if (bulletPos.x >= enemyPos.x - enemySize / 2 &&
+		bulletPos.y >= enemyPos.y - enemySize / 2 &&
+		bulletPos.x <= enemyPos.x + enemySize &&
+		bulletPos.y <= enemyPos.y + enemySize) {
+		isAlive = false;
+	}
+
+	if (isAlive == false) {
 		sceneNo = CLEAR;
 	}
+
 }
 
 void StageScene::Draw() {
+
+	Novice::DrawBox(int(playerPos.x), int(playerPos.y), 32, 32, 0.0f, WHITE, kFillModeSolid);
+	Novice::DrawBox(int(enemyPos.x), int(enemyPos.y), enemySize, enemySize, 0.0f, WHITE, kFillModeSolid);
+	Novice::DrawBox(int(bulletPos.x), int(bulletPos.y), bulletSize, bulletSize, 0.0f, WHITE, kFillModeSolid);
+
 	Novice::ScreenPrintf(0, 0, "SCENE : STAGE");
 	Novice::ScreenPrintf(0, 20, "Press[SPACE]");
 }
--- stageScene.h
+++ stageScene.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "IScene.h"
 #include <Novice.h>
+#include "Vector2.h"
 
 class StageScene : public IScene
 {
@@ -16,5 +17,19 @@ private:
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
+
+	Vector2 playerPos;
+	Vector2 enemyPos;
+
+	Vector2 bulletPos;
+
+	int bulletSize;
+	int enemySize;
+
+	float speed;
+
+	bool isAlive;
+	bool isBulletShot;
+
 };
 
